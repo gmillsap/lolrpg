dafb7ed67a0d35d2a7a206c62cdaae18ba2443cb
Andrew Watts <andrew@poweriep.com>
2016-04-28 13:00:44 -0500

    added 'screens' to home page

--- index.php
+++ index.php
@@ -5,11 +5,31 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     </head>
-    <body>
-        <div class="container">
+    <body background="/img/map1.png">
+        <div class="container" style="width: 900px; height:100%; background-color:white">
             <div class="row">
                 <div class="col-xs-12 text-center">
                     <h1>LOLRPG</h1>
+                </div>
+            </div>
+            <div class="row log-in-screen" style="background-color: lightblue; height: 100%">
+                <div class="container">
+                    <div class="row" style="padding-top: 10px">
+                        <div class="col-xs-2" style="padding-top: 4px;">
+                            <label class="form-label">Enter Summoner Name:</label>
+                        </div>
+                        <div class="col-xs-6">
+                            <input type="text" class="form-control" value="">
+                        </div>
+                        <div class="col-xs-2">
+                            <button class="btn btn-sm">Sign In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row champ-select-screen" style="background-color: green; height: 100%">
+                <div class="container">
+
                 </div>
             </div>
         </div>
