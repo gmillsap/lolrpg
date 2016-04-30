6785e5f1a9e5ce63a223700cab33ee6733494506
George Millsap <George Millsap>
2016-04-30 11:14:50 -0500

    Added findInput method

--- Class/LolRpg/Controllers/ControllerBase.php
+++ Class/LolRpg/Controllers/ControllerBase.php
@@ -6,4 +6,11 @@ class ControllerBase
     protected function returnAsJson($data) {
         echo json_encode($data);
     }
+
+    protected function findInput($index) {
+        if(isset($_REQUEST[$index])) {
+            return $_REQUEST[$index];
+        }
+        return '';
+    }
 }
