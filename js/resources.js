83facec00209914b5b8da64ffdc74d37002936c0
George Millsap <George Millsap>
2016-04-30 13:41:48 -0500

    Added method findSummonerData and method findSummonerMasteryData

--- js/resources.js
+++ js/resources.js
@@ -1,14 +1,71 @@
 $(function() {
     var Resources = function() {
-        this.findChampionData = function(champion_name) {
-            if(LOLRPG.game.use_test_data) {
-                console.log('here');
-                if(typeof LOLRPG.TEST_DATA.champions[champion_name] != 'undefined') {
-                    return LOLRPG.TEST_DATA.champions[champion_name]
+
+        this.findSummonerData = function(summoner_name, region, callback) {
+            var callback = callback || function() {};
+            var params = {
+                'url': '/Summoner/SummonerData',
+                'data': {
+                    'lol_region': region,
+                    'lol_summoner_name': summoner_name
                 }
-            } else {
-                //api
             }
+            /*summoner_mastery_data will be formatted like this:
+             {
+                 'summoner_data': {
+                     'id': integer - value for summoner id,
+                     'name': string - name of summoner,
+                     'profileIcondId': integer - profile icon id number,
+                     'revisionsDate': timestamp,
+                     'summonerLevel': integer - level of summoner
+                 }
+             }
+             */
+            LOLRPG.ajaxData.fetchData(params, function(summoner_data) {
+                callback(summoner_data);
+            });
+        }
+
+        this.findSummonerMasteryData = function(summoner_name, region, callback) {
+            var callback = callback || function() {};
+            var params = {
+                'url': '/Summoner/TopTenChampionMasteryData',
+                'data': {
+                    'lol_region': region,
+                    'lol_summoner_name': summoner_name
+                }
+            }
+            LOLRPG.ajaxData.fetchData(params, function(summoner_mastery_data) {
+                /*summoner_mastery_data will be formatted like this:
+                    {
+                        'champion_mastery_score': integer - value of summoner mastery score
+                        'summoner_data': {
+                            'id': integer - value for summoner id,
+                            'name': string - name of summoner,
+                            'profileIcondId': integer - profile icon id number,
+                            'revisionsDate': timestamp,
+                            'summonerLevel': integer - level of summoner
+                        },
+                        'top_ten_champion_mastery_data': {
+                            0: {
+                                'championId': integer - id of champion,
+                                'championLevel': integer - mastery level of champion,
+                                'championPoints': integer - total champion mastery points for champion,
+                                'championPointsSinceLastLevel': integer - champion points earned since last level,
+                                'championPointsUntilNextLevel': integer - champion points until next level,
+                                'chestGranted': boolean - champion S rating chest earned,
+                                'highestGrade': string - highest grade earned on champion,
+                                'lastPlayTime': timestamp - last time the champion was played,
+                                'playerId': integer - id - of player mastery belongs to
+                            1: {
+                                ...
+                            },
+                            ...
+                        }
+                    }
+                */
+                callback(summoner_mastery_data);
+            });
         }
     };
     LOLRPG.Resources = new Resources();
