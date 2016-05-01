05353d9651e2120cf9c69d4c91b032c6d23f59b6
George Millsap <George Millsap>
2016-05-01 00:01:37 -0500

    Added background image.

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
         <link rel="shortcut icon" href="/img/favicon.ico">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" href="http://bootswatch.com/slate/bootstrap.min.css">
+        <link rel="stylesheet" href="css/lolrpg.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        
     </head>
-    <body background="/img/map1.png">
+    <body>
         <div class="container" style="width: 900px; min-height: 100%; background-color: inherit">
             <div class="row">
                 <div class="col-xs-12 text-center">
