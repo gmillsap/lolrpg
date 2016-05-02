25e01a2288f3d1d27e907bd60295b372913dc754
George Millsap <George Millsap>
2016-05-01 20:26:32 -0500

    Added champion data to return set and checked for error.

--- Class/LolRpg/Controllers/Summoner.php
+++ Class/LolRpg/Controllers/Summoner.php
@@ -18,14 +18,38 @@ class Summoner extends ControllerBase
         $summoner = new SummonerByName($this->region);
         $summoner_name = strtolower($this->findInput('lol_summoner_name'));
         $base_summoner_data = $summoner->findSummonerData($summoner_name);
+        lolrpg_log($base_summoner_data);
+        if(empty($base_summoner_data) || !empty($base_summoner_data['error'])) {
+            return $this->returnAsJson(array('error' => 'invalid_summoner_name'));
+        }
         $mastery_score = new Score($this->region);
         $champion_mastery_score = $mastery_score->findForPlayer($base_summoner_data['id']);
+        if(empty($champion_mastery_score) || !empty($champion_mastery_score['error'])) {
+            return $this->returnAsJson(array('error' => 'empty_mastery_data'));
+        }
         $top_champ_mastery = new TopChampions($this->region);
         $top_ten_champ_mastery_data = $top_champ_mastery->findTopTenForPlayer($base_summoner_data['id']);
+        if(!empty($top_ten_champ_mastery_data['error'])) {
+            return $this->returnAsJson(array('error' => 'empty_mastery_data'));
+        }
+
+        $static_champion = new \LolRpg\Resources\StaticData\Champion($this->region);
+        $fetch_data = array(
+            'image',
+            'spells',
+            'stats',
+            'tags'
+        );
+        $all_static_champs = $static_champion->findStaticChampionData($fetch_data);
+        $champ_data = array();
+        foreach($top_ten_champ_mastery_data as $key => $champ_mastery_data) {
+            $champ_data[$champ_mastery_data['championId']] = $all_static_champs[$champ_mastery_data['championId']];
+        }
         $return_data = array(
             'summoner_data' => $base_summoner_data,
             'champion_mastery_score' => $champion_mastery_score,
-            'top_ten_champion_mastery_data' => $top_ten_champ_mastery_data
+            'top_ten_champion_mastery_data' => $top_ten_champ_mastery_data,
+            'champion_data' => $champ_data
         );
         return $this->returnAsJson($return_data);
     }
