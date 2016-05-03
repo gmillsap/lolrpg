4f2611d11f6f08fb850679313c6e0f5a14e24075
George Millsap <George Millsap>
2016-05-03 01:04:13 -0500

    Added one check and row checking.

--- js/config.js
+++ js/config.js
@@ -40,4 +40,32 @@ $(function() {
             this.callback();
         }
     }
+
+    LOLRPG.rowCheck = function(selector) {
+        $('body').off('click.row', selector).on('click.row', selector, function(e) {
+            console.log($(this));
+            var $checkbox = $(this).find('input[type="checkbox"]');
+            if($checkbox.length > 0) {
+                if($(this).not(':checkbox')) {
+                    $checkbox.trigger('click');
+                }
+            }
+        });
+    }
+    LOLRPG.rowCheck('.lolrpg-row-check');
+
+    LOLRPG.onlyOneCheck = function(selector) {
+        $('body').off('change.one_check', selector + ' input[type="checkbox"]').on('click.one_check', selector + ' input[type="checkbox"]', function() {
+            var $this = $(this);
+            if($this.is(':checked')) {
+                var $checkboxes = $(selector).find('input[type="checkbox"]');
+                $checkboxes.each(function() {
+                    if(!$(this).is($this)) {
+                        $(this).prop('checked', false);
+                    }
+                });
+            }
+        });
+    }
+    LOLRPG.onlyOneCheck('.lolrpg-only-one-check');
 });
